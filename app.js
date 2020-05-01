e4d53e69b5d90f151ba14fa839468771842b088b
Benjamin <spatialcontents@gmail.com>
2020-04-30 21:29:43 -0700

    Added the first renders of the product. Cleaned up some of the interface. Deleted old photos. Fixed desktop styles and typos in and for the nav menu

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 $(document).ready(function(){
 
-  $("#nav-toggle").click(function(){
+  $(".nav-toggle").click(function(){
     if ($(".nav").css("display")=="none"){
       $(".nav").css("display", "inherit");
       $(".nav").css("box-shadow", "-19px 0 76px rgba(0,0,0,0.30), -15px 0 24px rgba(0,0,0,0.22)");
-      $("#nav-toggle").removeClass("lnr lnr-menu");
-      $("#nav-toggle").addClass("lnr lnr-cross");
+      $(".nav-toggle").removeClass("lnr lnr-menu");
+      $(".nav-toggle").addClass("lnr lnr-cross");
     } else {
       $(".nav").css("display", "none");
-      $("#nav-toggle").removeClass("lnr lnr-cross");
-      $("#nav-toggle").addClass("lnr lnr-menu");
+      $(".nav-toggle").removeClass("lnr lnr-cross");
+      $(".nav-toggle").addClass("lnr lnr-menu");
     }
   })
 
   $(".nav>a").click(function(){
     $(".nav").css("display", "none");
-    $("#nav-toggle").removeClass("lnr lnr-cross");
-    $("#nav-toggle").addClass("lnr lnr-menu");
+    $(".nav-toggle").removeClass("lnr lnr-cross");
+    $(".nav-toggle").addClass("lnr lnr-menu");
   })
 
   $(window).scroll(function(){
@@ -25,7 +25,7 @@ $(document).ready(function(){
 
   $(function () {
   $(document).scroll(function () {
-    var $nav = $("#nav-toggle");
+    var $nav = $(".nav-toggle");
     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
   });
 });
